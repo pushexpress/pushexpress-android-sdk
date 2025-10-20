e60bb4da9ab3890252f3730ed42ebb9b9126854c
serresser <smpsimpletemplate@gmail.com>
2025-10-20 16:08:56 +0300

    v1.3.3

--- sdkpushexpress/src/main/java/com/pushexpress/sdk/notification_actions/NotificationClickBroadcastReceiver.kt
+++ sdkpushexpress/src/main/java/com/pushexpress/sdk/notification_actions/NotificationClickBroadcastReceiver.kt
@@ -3,31 +3,45 @@ package com.pushexpress.sdk.notification_actions
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import com.pushexpress.sdk.BuildConfig
 import com.pushexpress.sdk.main.SDK_TAG
 import com.pushexpress.sdk.notification.NotificationDrawerImpl.Companion.EXTRA_PX_MSG_ID
+import com.pushexpress.sdk.notification.NotificationDrawerImpl.Companion.EXTRA_PX_LINK
 import com.pushexpress.sdk.main.SdkPushExpress
 import com.pushexpress.sdk.models.NotificationEvent
 
 class NotificationClickBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
+        if (context == null || intent == null) {
+            Log.e(SDK_TAG, "Context or Intent is null")
+            return
+        }
         val pxMsgId = intent?.getStringExtra(EXTRA_PX_MSG_ID) ?: ""
+        val deepLink = intent.getStringExtra(EXTRA_PX_LINK)
+
         SdkPushExpress.sdkApi.sendNotificationEvent(pxMsgId, NotificationEvent.CLICK)
 
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+        if (!deepLink.isNullOrEmpty()) {
+            startDeepLinkIntent(context, deepLink)
+        } else {
             startDefaultActivity(context)
         }
-        /* TODO: define clean openlink workflow
-        else {
-            startOpenLinkIntent(context, intent?.getStringExtra(EXTRA_PX_LINK))
-        }*/
+
         if (BuildConfig.LOG_DEBUG) Log.d(SDK_TAG, "Clicked: $pxMsgId")
     }
 
     override fun peekService(myContext: Context?, service: Intent?): IBinder {
         return super.peekService(myContext, service)
+    }
+
+    private fun startDeepLinkIntent(context: Context, deepLink: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(deepLink))
+        intent.setPackage(context.packageName)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        context.startActivity(intent)
     }
 }
--- sdkpushexpress/src/main/java/com/pushexpress/sdk/notification_actions/TrampolineActivity.kt
+++ sdkpushexpress/src/main/java/com/pushexpress/sdk/notification_actions/TrampolineActivity.kt
@@ -1,6 +1,9 @@
 package com.pushexpress.sdk.notification_actions
 
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -9,20 +12,31 @@ import com.pushexpress.sdk.main.SDK_TAG
 import com.pushexpress.sdk.main.SdkPushExpress
 import com.pushexpress.sdk.models.NotificationEvent
 import com.pushexpress.sdk.notification.NotificationDrawerImpl.Companion.EXTRA_PX_MSG_ID
-
+import com.pushexpress.sdk.notification.NotificationDrawerImpl.Companion.EXTRA_PX_LINK
 class TrampolineActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val extras = intent?.extras
         val pxMsgId = extras?.getString(EXTRA_PX_MSG_ID).orEmpty()
+        val deepLink = extras?.getString(EXTRA_PX_LINK)
+
         SdkPushExpress.sdkApi.sendNotificationEvent(pxMsgId, NotificationEvent.CLICK)
 
-        startDefaultActivity(this.applicationContext)
-        // TODO: define clean openlink workflow
-        // startOpenLinkIntent(this, extras?.getString(FcmService.EXTRA_PX_LINK))
+        if (!deepLink.isNullOrEmpty()) {
+            startDeepLinkIntent(this, deepLink)
+        } else {
+            startDefaultActivity(this.applicationContext)
+        }
 
         if (BuildConfig.LOG_DEBUG) Log.d(SDK_TAG, "Clicked: $pxMsgId")
         finish()
+    }
+
+    private fun startDeepLinkIntent(context: Context, deepLink: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(deepLink))
+        intent.setPackage(context.packageName)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        context.startActivity(intent)
     }
 }
